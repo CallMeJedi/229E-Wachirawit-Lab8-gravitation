9e79db9382b80c0568ab1293c92de6e3c4e72d66
CallMeJedi <jediv5plus@gmail.com>
2025-03-13 10:49:36 +0700

    Update GravityE.cs

--- Assets/Scripts/GravityE.cs
+++ Assets/Scripts/GravityE.cs
@@ -1,14 +1,50 @@
 using System;
+using NUnit.Framework;
+using System.Collections.Generic; //สร้างตู้
 using UnityEngine;
 
 public class GravityE : MonoBehaviour
 {
     Rigidbody rb;
-    private const float G = 0.00674f;
+    const float G = 0.06674f;
+    public static List<GravityE> gravityObjList; //สร้างตู้
+
+    //Orbitting
+    [SerializeField] bool planets = false;
+    [SerializeField] int orbitSpeed = 1000;
+
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+
+        //สร้างตู้>>>
+        if (gravityObjList == null) 
+        {
+            gravityObjList = new List<GravityE>();
+        }
+        gravityObjList.Add(this);
+        //<<<สร้างตู้
+        
+        //Orbitting
+        if (!planets)
+        {
+            rb.AddForce(Vector3.left * orbitSpeed);
+        }
+        //Orbitting
+    }
+
+    private void FixedUpdate()
+    {
+        //Attract
+        foreach (var OBJPlanets in gravityObjList)
+        {
+            if (OBJPlanets != this)
+            {
+                Attract(OBJPlanets);
+            }
+        }
+        //Attract
     }
 
     void Attract(GravityE other)
